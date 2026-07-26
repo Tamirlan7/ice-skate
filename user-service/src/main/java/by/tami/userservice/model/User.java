138cf56aad8c287e0db122161ff16ff1a2c27beb
Tamirlan7 <zukasaevtamirlan1@gmail.com>
2026-07-26 10:21:55 +0500

    minor changes to user service

--- user-service/src/main/java/by/tami/userservice/model/User.java
+++ user-service/src/main/java/by/tami/userservice/model/User.java
@@ -1,13 +1,33 @@
 package by.tami.userservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "t_user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "phone_number", unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
